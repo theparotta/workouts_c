d83539ef09d2536db9ec3738e2d814e7d3ec1cca
TheParotta <126277487+theparotta@users.noreply.github.com>
2023-11-21 09:21:17 +0530

    functions exercise

---
+++ number_to_roman.c
@@ -0,0 +1,28 @@
+/*
+ * I - 1, V - 5, X - 10, L - 50, C - 100, D - 500, M - 1000
+ */
+
+#include <stdio.h>
+
+int main() {
+  int POSITIONS[] = {1000, 500, 100, 50, 10, 5, 1};
+  char ROMAN[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+  
+  int num;
+  
+  puts("Enter a positive integer: ");
+  scanf("%d", &num);
+  
+  for (int i = 0; i < sizeof(ROMAN)/sizeof(ROMAN[0]); i++) {
+    int place_holder = num / POSITIONS[i];
+    
+    for (int j = place_holder; j > 0; j--)
+      printf("%c", ROMAN[i]);
+    
+    // updating the num value with the remainder
+    num %= POSITIONS[i];
+  }
+  
+  printf("\n");
+  return(0);
+}
